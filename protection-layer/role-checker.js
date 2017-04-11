d614e0139e927c9237d242b80b18d582efa01d25
igorgoremykin <c-sharp@bk.ru>
2017-04-11 03:20:04 +0300

    Added some code to RoleChecker

--- protection-layer/role-checker.js
+++ protection-layer/role-checker.js
@@ -3,13 +3,21 @@
 const CallInterceptor = require('../call-interceptor');
 
 class RoleChecker extends CallInterceptor {
-  constructor(source) {
+  constructor(source, permissionsScheme) {
     super(source);
+    this.permissionsScheme = permissionsScheme;
   }
 
   before(methodName, userId) {
-    let userRole = this.getUserRole(userId);
     let sourceType = this.getSourceType();
+
+    if (userId) {
+      let userRole = this.getUserRole(userId);
+
+      throw new Error('Forbidden');
+    }
+
+    throw new Error('Unauthorized');
   }
 
   getUserRole(id) {
