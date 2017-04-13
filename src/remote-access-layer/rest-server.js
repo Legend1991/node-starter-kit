be1b122bc7e937b25716a62f1b710eef7248c518
Igor Goremykin <c-sharp@bk.ru>
2017-04-13 17:15:23 +0300

    Update rest-server.js
    Fixes for rest server

--- src/remote-access-layer/rest-server.js
+++ src/remote-access-layer/rest-server.js
@@ -7,11 +7,54 @@ const ForbiddenError = require('../errors/forbidden-error');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 class RestServer {
-  constructor(serverConfig, ...args) {
-
+  constructor(config, ...args) {
+      this.init(config, args);
   }
 
-  buildApiRoute(apiScheme, sources) {
+  init(config, sources) {
+      let server = this.buildServer();
+      let apiRouter = this.buildApiRouter(config.apiScheme, sources);
+      this.configureServer(server, apiRouter);
+      this.runServer(server, config.runConfig);
+  }
+
+  buildServer() {
+      return express();
+  }
+
+  configureServer(server, apiRouter) {
+      server.use(morgan('dev'));
+      server.use(bodyParser.urlencoded({extended: true}));
+      server.use(bodyParser.json({limit: '100mb'}));
+      server.use(compression({threshold: 0}));
+      server.use(express.static(path.resolve('./frontend/public/')));
+      server.engine('html', consolidate['swig']);
+      server.set('view engine', 'html');
+      server.set('views', path.resolve('./frontend/src/'));
+      server.use('/', apiRouter);
+      server.use((error, req, res, next) => {
+          let err = {
+              message: error.toString()
+          };
+
+          if (error.name === 'ValidationError') {
+              err = {
+                  message: error.name,
+                  errors:  error.errors
+              }
+          } else {
+              console.error('Error: ', error);
+          }
+
+          res.status(500).json(err);
+      });
+  }
+
+  runServer(server, runConfig) {
+      server.listen(runConfig.port, () => console.log('Server started on port: ', runConfig.port));
+  }
+
+  buildApiRouter(apiScheme, sources) {
     let apiRouter = Router();
 
       sources.forEach((source) => {
@@ -33,7 +76,7 @@ class RestServer {
         }
       });
 
-      return apiRouter;
+    return apiRouter;
   }
 }
 
