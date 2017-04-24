2acb09646094896e45e8db3da103687f37ee91b5
Igor Goremykin <c-sharp@bk.ru>
2017-04-24 17:53:37 +0300

    Update request-handler.js
    Added args extraction from request

--- src/remote-access-layer/request-handler.js
+++ src/remote-access-layer/request-handler.js
@@ -10,7 +10,13 @@ class RequestHandler extends CallInterceptor {
   }
 
   async before(methodName, req, res, next) {
+    let args = [];
 
+    args = args.concat(Object.values(req.params));
+    args = args.concat(Object.values(req.query));
+    args.push(req.body);
+
+    return args;
   }
 
   async after(methodName, targetResult, req, res, next, ...args) {
